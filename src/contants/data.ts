358a94fe60022e75637b57143b892821e446d3bb
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-16 23:42:52 -0300

    added types to project

---
+++ src/contants/data.ts
@@ -0,0 +1,23 @@
+// struture of teh data
+export interface Recipe {
+  imageUrl: string;
+  title: string;
+  subText: string;
+  preparationTime: {
+    total: string;
+    preparation: string;
+    cookingTime: string;
+  };
+  ingredients: string[];
+  instructions: {
+    title: string;
+    description: string;
+  };
+  nutrition: {
+    subtext: string;
+    table: {
+      title: string;
+      quantity: string;
+    };
+  };
+}
