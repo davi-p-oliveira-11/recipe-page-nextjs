da179b3dd1dc2eabe18379f58ee416726d55b928
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-17 22:09:57 -0300

    added tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,27 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        white: "#ffffff",
+        background: "#f3e5d7",
+        stone150: "#e3ddd7",
+        stone600: "#5f564e",
+        stone900: "#302d2b",
+        brown800: "#854532",
+        rose800: "#7b274e",
+        rose50: "#fff4f9",
       },
+      fontFamily: {
+        youngSerif: ["Young Serif", "sans-serif"],
+        outfit: ["Outfit", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
     },
   },
   plugins: [],
